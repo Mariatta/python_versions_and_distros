b75d61388cc59af5c37c4f2a62fecd067928bdb7
Thomas A Caswell <tcaswell@gmail.com>
2017-10-15 12:43:57 -0400

    ENH: search for 3.4, 3.5, and 3.7 as well

--- py_versions_and_distros.py
+++ py_versions_and_distros.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 
 CSV_FIELDNAMES = ['distribution', 'dist_version', 'python_version', 'resource']
-PY_3_6_VERSIONS = ["3.6.2",
-                   "3.6.1",
-                   "3.6.0"]
+PY_VER_MAP = {py_ver: [f'{py_ver}.{j}' for j in range(15)]
+              for py_ver in ('3.4', '3.5', '3.6', '3.7')
+              }
 
 
 def fetch_webpage(source_url, destination_filename):
@@ -33,6 +33,7 @@ def scrape_webpage(source_url, destination_filename):
         os.path.getmtime(destination_filename))
     if mod_date.date() < datetime.today().date():
         fetch_webpage(source_url, destination_filename)
+
 
 def get_distros():
     """
@@ -60,14 +61,15 @@ def process_distro(distribution):
             for sib in th[0].next_siblings:
                 a_elem = sib.find("a")
                 if a_elem and a_elem != -1:
-                    process_resource(distribution, a_elem.text)
+                    for py_ver in PY_VER_MAP:
+                        process_resource(distribution, a_elem.text, py_ver)
 
 
-def process_resource(distribution, dist_version):
-    """
-    Download the resources at
+def process_resource(distribution, dist_version, py_ver):
+    """Download the resources at
     https://distrowatch.com/resource/{distribution}/{distribution}-{dist_version}.txt
-    It lists all the packaged shipped for the specified distribution and release.
+    It lists all the packaged shipped for the specified distribution
+    and release.
 
     Python 3.6 is possibly identified using one of the following patterns:
     These are only my own guess and assumption.
@@ -78,27 +80,32 @@ def process_resource(distribution, dist_version):
         python 3.6*     e.g. gentoo unstable: python 3.6.1
         python-3.6*     e.g arch current: python-3.6.2-1-i686.pkg.tar.xz
         python3.6*      e.g parrotsecurity 3.7: python3.6^3.6.2~rc1-1
+
     """
     resource_url = f"https://distrowatch.com/resource/{distribution}/{distribution}-{dist_version}.txt"
     resource_file_path = f"downloaded_resources_data/{distribution}-{dist_version}.txt"
 
     scrape_webpage(resource_url, resource_file_path)
 
+    search_patterns = [f"python3 {py_ver}",
+                       f"python3^{py_ver}",
+                       f"python3-{py_ver}",
+                       f"python{py_ver}",
+                       f"python {py_ver}",
+                       f"python-{py_ver}"]
     with open(resource_file_path) as file:
         for line in file.readlines():
-            if line.startswith("python3 3.6") or line.startswith("python3^3.6") \
-                    or line.startswith("python3-3.6") or line.startswith("python3.6") \
-                    or line.startswith("python 3.6") or line.startswith("python-3.6"):
+            if any(line.startswith(pattern) for pattern in search_patterns):
                 version = None
-                for v in PY_3_6_VERSIONS:
+                for v in PY_VER_MAP[py_ver]:
                     if v in line:
                         version = v
 
                 if version:
                     write_output({"distribution": distribution,
-                                   "dist_version": dist_version,
-                                   "python_version": version,
-                                   "resource": line.strip()})
+                                  "dist_version": dist_version,
+                                  "python_version": version,
+                                  "resource": line.strip()}, py_ver)
                     return
 
 
@@ -107,19 +114,19 @@ def init_csv():
     Clean up the output csv (python_distros.csv)
     Create the output csv file, along with the headers
     """
-    if os.path.exists("python_distros.csv"):
-        os.remove("python_distros.csv")
+    for py_ver in PY_VER_MAP:
+        path = Path(f"python_distros_{py_ver}.csv")
 
-    with open("python_distros.csv", 'a') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=CSV_FIELDNAMES)
-        writer.writeheader()
+        with open(path, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=CSV_FIELDNAMES)
+            writer.writeheader()
 
 
-def write_output(dist_json):
+def write_output(dist_json, py_ver):
     """
     Write a line to python_distros.csv
     """
-    with open("python_distros.csv", 'a') as csvfile:
+    with open(f"python_distros_{py_ver}.csv", 'a') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=CSV_FIELDNAMES)
         writer.writerow(dist_json)
 
@@ -128,26 +135,36 @@ def print_report():
     """
     Print to terminal the distributions that have python 3.6
     """
-    with open("python_distros.csv") as file:
-        csvreader = csv.DictReader(file, fieldnames=CSV_FIELDNAMES)
-        count = 0
-        for row in csvreader:
-            if count > 0:
-                print(f"{row['distribution']} {row['dist_version']}: Python {row['python_version']}")
-            count += 1
-        print("="*36)
-        print(f"{count-1} distros with Python 3.6")
+    for py_ver in PY_VER_MAP:
+        data_fpath = Path(f"python_distros_{py_ver}.csv")
+        if not data_fpath.exists():
+            continue
+        with open(data_fpath) as file:
+            csvreader = csv.DictReader(file, fieldnames=CSV_FIELDNAMES)
+            count = 0
+            for row in csvreader:
+                if count > 0:
+                    print(f"{row['distribution']} {row['dist_version']}: Python {row['python_version']}")
+                count += 1
+            print("="*36)
+            print(f"{count-1} distros with Python {py_ver}")
+
+        print('\n'*2)
+
 
 def main():
     init_csv()
-    scrape_webpage('https://distrowatch.com/', "downloaded_data/distrowatch.html")
+    scrape_webpage('https://distrowatch.com/',
+                   "downloaded_data/distrowatch.html")
     for distro in get_distros():
         distro_url = f"https://distrowatch.com/table-mobile.php?distribution={distro}"
         dest_filename = f"downloaded_data/{distro}.html"
         scrape_webpage(distro_url, dest_filename)
         process_distro(distro)
 
+
     print_report()
+
 
 if __name__ == "__main__":
     main()
